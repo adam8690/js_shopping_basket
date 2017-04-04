375bf7d52d47993a9828036e029553a2ffea4c2f
Adam <adam8690@hotmail.co.uk>
2017-04-04 14:51:52 +0100

    created item object and tested it had a name.

---
+++ item.js
@@ -0,0 +1,7 @@
+var Item = function(name, price, isBogof){
+  this.name = name;
+  this.price = price;
+  this.isBogof = isBogof;
+}
+
+module.exports = Item;
---
+++ specs/itemSpec.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var Item = require('../item.js');
+
+describe('Item', function(){
+
+  var item;
+  beforeEach(function(){
+    item = new Item("milk", 100, true);
+  })
+
+  it('should have a name', function(){
+    assert.strictEqual("milk", item.name);
+  })
+
+  it('should have price', function(){
+    assert.strictEqual(100, item.price);
+  })
+
+  
+
+
+})
